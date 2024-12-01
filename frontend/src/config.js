1b2d98804aaf9c156843a54ea67b534592bca3d1
thankyou0 <parthivva227@gmail.com>
2024-12-01 14:25:48 +0530

    1

---
+++ frontend/src/config.js
@@ -0,0 +1,19 @@
+
+
+const config = {
+  // BACKEND_API: process.env.REACT_APP_BACKEND_API,
+  // PWD_SECRET: process.env.REACT_APP_PWD_SECRET
+  // BACKEND_API :'https://deploy-news.vercel.app',
+  // BACKEND_API:'https://netlify-news-backend.netlify.app',
+
+  BACKEND_API: 'https://normal-backend-render.onrender.com',
+  BACKEND_API_SCRAP: 'https://normal-backend.vercel.app',
+  
+  // BACKEND_API: 'http://localhost:9000',
+  // BACKEND_API_SCRAP: 'http://localhost:9000',
+
+  PWD_SECRET: 'news-aggregator-secret'
+};
+
+
+export default config
