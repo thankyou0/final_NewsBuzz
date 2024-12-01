0a6920738e0ac16006b94b44ba20a6c1e2ada0c8
thankyou0 <parthivva227@gmail.com>
2024-12-01 15:04:26 +0530

    b

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -6,8 +6,11 @@ const config = {
   // BACKEND_API :'https://deploy-news.vercel.app',
   // BACKEND_API:'https://netlify-news-backend.netlify.app',
 
-  BACKEND_API: 'https://normal-backend-render.onrender.com',
-  BACKEND_API_SCRAP: 'https://normal-backend.vercel.app',
+  // BACKEND_API: 'https://normal-backend-render.onrender.com',
+  // BACKEND_API_SCRAP: 'https://normal-backend.vercel.app',
+
+  BACKEND_API: 'https://final-newsbuzz-backend.onrender.com',
+  BACKEND_API_SCRAP: 'https://final-news-buzz.vercel.app/',
   
   // BACKEND_API: 'http://localhost:9000',
   // BACKEND_API_SCRAP: 'http://localhost:9000',
